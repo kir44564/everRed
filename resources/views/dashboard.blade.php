f9838ac7c0de09bfc08fa0e7cb343fad1ef01dfd
kir44564 <kuchmakrakirill@gmail.com>
2025-05-22 17:39:01 +0200

    responsive table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,7 @@
 <h1>{{ Auth::user()->name }}, welcome to your Dashboard</h1>
 @endauth
 
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="stylesheet" href="{{ asset('/css/dash.css') }}">
 
 
